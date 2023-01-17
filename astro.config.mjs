15d80b2326a974caee7b70aa76e070bf0bb32fe6
n-ao <maroon.nopia@gmail.com>
2023-01-17 23:32:58 +0900

    [add] 今日のメモ

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://n-ao.github.io',
-    base: '/Astro-Getting-Started',
+    base: '/Astro-My-Blog',
 });
